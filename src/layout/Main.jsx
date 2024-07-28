28dded3ae227837bb7b795ddad7a4f21f4333086
ahmed-H20 <ahmedheshamahah8@gmail.com>
2024-07-29 00:20:17 +0300

    Edit file

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
   return (
     <div>
         <Nav/>
-        <div className='min-h-screen'>
+          <div className='min-h-screen'>
         <Outlet/>
         </div>
         <Footer/>
