583b82cad00e333aa6076c3762f744507377ae1d
ahmed-H20 <ahmedheshamahah8@gmail.com>
2024-07-29 04:49:21 +0300

    Edit close button

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -16,7 +16,9 @@ const Modal = () => {
             <div className="modal-box">           
                 <div className="modal-action flex flex-col justify-center mt-0 ">
                     <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
-                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                    htmlFor = "my_modal_5"
+                    onClick={()=>document.getElementById('my_modal_5').close()}>✕</button>
                         <h3 className="font-bold text-lg">Login please!</h3>
                         <div className="form-control">
                             <label className="label">
