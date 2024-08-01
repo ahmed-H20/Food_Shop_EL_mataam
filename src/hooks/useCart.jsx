2dfb384bb1bdfc592f492a7dc4c7a1aff6bf7666
ahmed-H20 <ahmedheshamahah8@gmail.com>
2024-08-01 21:29:24 +0300

    Edit page

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../contexts/AuthProvider'
+import { useQuery } from '@tanstack/react-query';
 
 const useCart = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const {user} = useContext(AuthContext);
+    const {refetch, data: cart = []} = useQuery({
+        queryKey: ['carts', user?.email],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:6001/carts?email=${user?.email}`);
+            if (!res.ok) {
+                throw new Error('Network response was not ok')
+            }
+            return res.json()
+        },
+    })
+    return [cart, refetch]
 }
 
 export default useCart
